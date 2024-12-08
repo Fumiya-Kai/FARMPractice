f716f68f0cbae148fa04e8959a6391bfaff95604
Fumiya-Kai <bunjyo.science@icloud.com>
2024-12-08 22:09:39 +0900

    add: todo一覧の取得APIを実装

--- routers/route_todo.py
+++ routers/route_todo.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 from fastapi import Request, Response, HTTPException
 from fastapi.encoders import jsonable_encoder
-from database import db_create_todo
+from database import db_create_todo, db_get_todos
 from schemas import Todo, TodoBody
 from starlette.status import HTTP_201_CREATED
+from typing import List
 
 router = APIRouter()
 
@@ -15,3 +16,8 @@ async def create_todo(request: Request, response: Response, data: TodoBody):
   if res:
     return res
   raise HTTPException(status_code=404, detail="Create task failed")
+
+@router.get("/api/todos", response_model=List[Todo])
+async def get_todos():
+  todos = await db_get_todos()
+  return todos
