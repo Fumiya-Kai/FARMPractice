07f07383af759cce04652966c92289d336e4ae73
Fumiya-Kai <bunjyo.science@icloud.com>
2024-12-31 00:09:23 +0900

    fix: python3.8用に修正

--- auth_utils.py
+++ auth_utils.py
@@ -49,7 +49,7 @@ class AuthJwtCsrf():
     subject = self.decode_jwt(value)
     return subject
 
-  def verify_update_jwt(self, request) -> tuple[str, str]:
+  def verify_update_jwt(self, request) -> tuple:
     subject = self.verify_jwt(request)
     new_token = self.encode_jwt(subject)
     return new_token, subject
